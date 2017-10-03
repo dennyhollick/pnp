bbed34b586d30b1b455383b0bef26a992f94b75c
Denny Hollick <dennyhollick@gmail.com>
2017-10-03 13:47:04 -0700

    Created sample fade-in script

---
+++ wp-content/themes/raysent/js/animation-loader.js
@@ -0,0 +1,10 @@
+
+// This is the function that loads animations as needed
+
+jQuery(document).ready(function( $ ) {
+	
+  setTimeout(function () {
+    $('.logo-text').show().addClass('fade-in');}, 2000
+  );
+	
+});
