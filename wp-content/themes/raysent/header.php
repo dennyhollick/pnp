f39ba3aa53a1764c71ce0efcabd079636341a0fc
Denny Hollick <dennyhollick@gmail.com>
2017-10-02 23:03:41 -0700

    Added main components to header

--- wp-content/themes/raysent/header.php
+++ wp-content/themes/raysent/header.php
@@ -16,43 +16,49 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+<div id="page" class="site section-blue">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'raysent' ); ?></a>
-	<div class="max-width-wrapper">
-	<header id="masthead" class="site-header section-blue">
-		<h5 id="masthead_left">Contact</h5>
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+		<div class="max-width-wrapper">
+			<!-- #masthead -->
+			<header id="masthead" class="site-header">
+				
+				<div class="pure-g">
+					
+					<!-- left-links -->
+					<div id="masthead-left" class="pure-u-1-3 fade-in">
+						<h6>Contact</h6>
+					</div>
+					
+					<!-- .site-branding -->
+					<div class="site-branding pure-u-1-3">
+						<div class="site-logo">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 258.5 321" style=""><path class="site-logo-svg STqAkDNk_0" d="M256.5 4.8L14.3 94l237 95v124.4l-237-94.9 242.4-89.2" style="max-width:50%;padding:0 5px 0 5px" stroke-dasharray="1152, 1154" fill="none" stroke="#F2EFDE" stroke-width="10" stroke-miterlimit="10"></path><style>.STqAkDNk_0{stroke-dasharray:1152 1154;stroke-dashoffset:1153;animation:STqAkDNk_draw 1333ms linear 0ms forwards;}@keyframes STqAkDNk_draw{100%{stroke-dashoffset:0;}}@keyframes STqAkDNk_fade{0%{stroke-opacity:1;}93.54838709677419%{stroke-opacity:1;}100%{stroke-opacity:0;}}</style></svg>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 258.5 321" style=""><path class="site-logo-svg WegPqqxq_0" d="M2 316.2L244.2 227 7.1 132V7.6l237.1 94.9L1.8 191.7" style="max-width:50%;padding:0 5px 0 5px" stroke-dasharray="1152, 1154" fill="none" stroke="#F2EFDE" stroke-width="10" stroke-miterlimit="10"></path><style>.WegPqqxq_0{stroke-dasharray:1152 1154;stroke-dashoffset:1153;animation:WegPqqxq_draw 1333ms linear 0ms forwards;}@keyframes WegPqqxq_draw{100%{stroke-dashoffset:0;}}@keyframes WegPqqxq_fade{0%{stroke-opacity:1;}93.54838709677419%{stroke-opacity:1;}100%{stroke-opacity:0;}}</style></svg>
+						</div>
+						<div>
+							<h4 class="logo-text">RAYSENT</h4>
+						</div>
+					</div>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+					<!-- right-links -->
+					<div id="masthead-right" class="pure-u-1-3 alignright">
+						<h6>EN | 中文</h6>
+					</div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'raysent' ); ?></button>
-			<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-	</div>
+				</div>
+			</header><!-- #masthead-end -->
+		</div>
 
-	<div id="content" class="site-content">
+	<div id="content" class="site-content section-blue">
+
+	<script type="text/javascript">
+        function codeAddress() {
+					new Vivus('site_logo_left', {duration: 200}, myCallback);
+					new Vivus('site_logo_right', {duration: 200}, myCallback);
+        }
+        window.onload = codeAddress;
+        </script>
