f0a93d77a5188906386aae9cc1b9a26ecc6bac21
Denny Hollick <dennyhollick@gmail.com>
2017-10-03 21:21:36 -0700

    Added animation function for main svg

--- wp-content/themes/raysent/js/animation-loader.js
+++ wp-content/themes/raysent/js/animation-loader.js
@@ -4,6 +4,8 @@
 
 jQuery(document).ready(function( $ ) {
 
+  // This loads the text after SVGs have animated for 2s
+
   $('.second').addClass('not-active');
 	
   setTimeout(function () {
@@ -12,5 +14,48 @@ jQuery(document).ready(function( $ ) {
   setTimeout(function () {
     $('.second').removeClass('not-active');}, 2000
   );
+  $('.third').addClass('not-active');
+
+  setTimeout(function () {
+    $('.third').addClass('fade-in');}, 3000
+  );
+  setTimeout(function () {
+    $('.third').removeClass('not-active');}, 5000
+  );
+
+
+  // This will calculate the distance between the main title and your mouse
+
+  (function() {
+    
+    var mX, mY, distance,
+        $distance = $('#distance span'),
+        $element  = $('#Layer_1');
+
+    function calculateDistance(elem, mouseX, mouseY) {
+        return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
+    }
+
+    function calculateBrightness (distance) {
+      var maxDist = 800;
+      var precentageOfMaxDist = (800 - distance) / 800;
+      if (precentageOfMaxDist > .10) {
+        return precentageOfMaxDist;
+      } else {
+        return .10;
+      }
+    }
+
+    $(document).mousemove(function(e) {  
+        mX = e.pageX;
+        mY = e.pageY;
+        distance = calculateDistance($element, mX, mY);
+        var brightness = calculateBrightness(distance);
+        var shadowString = "drop-shadow(rgba(255, 255, 255, " + brightness + ") 0px 0px 6px) "
+        var shadowStringConcat = shadowString + shadowString + shadowString;
+        $element.attr('style', 'filter: ' + shadowStringConcat);
+    });
+
+})();
 	
 });
